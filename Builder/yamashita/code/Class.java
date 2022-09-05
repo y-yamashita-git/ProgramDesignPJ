8adfa7f28d20c7fe7016d3c71d1022023f5e5adf
y-yamashita-git <yoshino.yamashita.9a@gmail.com>
2022-09-05 22:47:20 +0900

    Delete：前回ディレクトリBuilderファイル削除

--- Builder/yamashita/code/Class.java
+++
@@ -1,5 +0,0 @@
-class Class{
-    fun setTeacher(string) : void
-    fun setStudents(array) : void
-    fun setSubTeacher(string) : void
-}
--- Builder/yamashita/code/ClassWiithoutSubTeacher.java
+++
@@ -1,7 +0,0 @@
-class ClassWithSubTeacher extends Class{
-    @Override
-    fun setTeacher(string) : void
-
-    @Override
-    fun setStudents(array) : void
-}
